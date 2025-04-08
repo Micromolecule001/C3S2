23813a5b43bb751f6fc83c0bf72eace29e649333
Micromolecule001 <dima.boyko.bd@gmail.com>
2025-04-08 08:20:38 +0300

    to21

---
+++ apps/1StringAnalyzer/app/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        MainFrame.main(args);
+    }
+
+    // Added getGreeting method
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
+
