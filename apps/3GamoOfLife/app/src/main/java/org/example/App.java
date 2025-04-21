f2ac6c18d727f4684d9dd12c6d450b0ae6e8dc7e
Micromolecule001 <dima.boyko.bd@gmail.com>
2025-04-21 22:14:49 +0300

    4

--- apps/3GamoOfLife/app/src/main/java/org/example/App.java
+++ apps/3GamoOfLife/app/src/main/java/org/example/App.java
@@ -12,11 +12,13 @@ public class App {
             JFrame frame = new JFrame("Game of Life");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             int[][] grid = new int[50][50];
+
             // Optionally, initialize some cells to be alive
             grid[24][24] = 1;
             grid[24][25] = 1;
             grid[24][26] = 1;
             grid[25][25] = 1;
+
             GridPanel gridPanel = new GridPanel(grid);
             frame.add(gridPanel);
             frame.pack();
