cffb286d8e41bbe4fe79f1c111bdf49e44bd293a
Micromolecule001 <dima.boyko.bd@gmail.com>
2025-04-23 20:13:54 +0300

    internet techno...

--- apps/2UnitCoverter/app/src/main/java/org/example/App.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class App {
-    public static void main(String[] args) {
-        // This can delegate to MainFrame or handle additional startup logic.
-        MainFrame.main(args);
-    }
-}
-
