778e8ae3015fd088bf84768a6f7071b18d26d2ed
Micromolecule001 <dima.boyko.bd@gmail.com>
2025-04-25 23:15:53 +0300

    lab2

--- lab2/client.py
+++ lab2/client.py
@@ -3,7 +3,8 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # IP-адреса сервера
-server_address = ('127.0.0.1', 12345)
+# server_address = ('127.0.0.1', 12345)
+server_address = ('192.168.0.104', 12345)
 
 try:
     while True:
